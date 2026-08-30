c67f9a6a56e6b03ad91613379b30802d370ef424
mcknzryn <webmaster@mckenzieryan.co>
2026-08-30 18:22:52 -0400

    home page uses same galleryOrder as work page, now housed in work-gallery.ts

---
+++ src/data/work-gallery.ts
@@ -0,0 +1,5 @@
+// Keep the production arrangement easy to edit while allowing Arrange mode to
+// generate the next version of this list visually.
+export const galleryOrder = [
+  "29", "16", "01", "20", "34", "13", "00", "41", "35", "25", "46", "99", "22", "18", "26", "45", "09", "44", "23", "15", "11", "42", "02", "28", "10", "32", "06", "30", "31", "14", "12", "17", "37",
+];
